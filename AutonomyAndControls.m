f478c3330b427b582648e22c46d3e01afb14784f
Klaudia Sarat <112783872+klaudiasarat@users.noreply.github.com>
2023-03-27 14:06:16 -0700

    cleaned up code; added instructions/comments

--- AutonomyAndControls.m
+++ AutonomyAndControls.m
@@ -1,16 +1,24 @@
-% 
+% Not using right now - may modify or come back to this later 
+%        startMoving = 0;
+ %       rightTurns = 0;
+  %      leftTurns = 0;
+   %     counter = 0;
 
 
-startMoving = 0;
-rightTurns = 0;
-leftTurns = 0;
-counter = 0;
-
-brick.SetColorMode(2,2);
-
+% ------------------------------------------------------------------------------------------------
 global key
+
 InitKeyboard();
-% brick = ConnectBrick('OLLIE');
+
+manualControls = false;
+    % Manual controls will be used in green and blue areas to pick up and drop off passenger model
+    % Full manual controls detailed below:
+        % Move forward: uparrow
+        % Move backward: downarrow
+        % Turn left: 
+
+
+
 
 while 1
     pause(0.1);
@@ -18,6 +26,7 @@ while 1
     touch = brick.TouchPressed(3);
     touch1 = brick.TouchPressed(1);
     color = brick.ColorCode(2);
+
 
     switch key
 
@@ -41,46 +50,49 @@ while 1
                     startMoving = 0;
                 end
 
+% ------------------------------------------------------------------------------------------------
 % As of 3/27/23, the vehicle would not work as expected
-% Testing before implementing autonomous instructions/code: 
+% Testing before implementing autonomous instructions/code:
 
+% ------------------------------------------------------------------------------------------------
 % MANUAL CONTROLS
     % Move Forward
                 if (key == uparrow)
-                    brick.MoveMotor('B', 50);
-                    brick.MoveMotor('C', 50);
+                    brick.MoveMotor('BC', 50);
                 end
                 
     % Move Backward
                 if (key == downarrow)
-                    brick.MoveMotor('B', -50);
-                    brick.MoveMotor('C', -50);
+                    brick.MoveMotor('BC', -50);
                 end
 
     % Turn Left
                 if (key == leftarrow)
-                    brick.MoveMotor('B', -45);
-                    brick.MoveMotor('C', 45);
+                    brick.MoveMotor('B', 45);
+                    brick.MoveMotor('C');
                 end
                     
     % Turn Right
                 if (key == reightarrow)
-                    brick.MoveMotor('B', 45);
-                    brick.MoveMotor('C', -45);
+                    brick.MoveMotor('B');
+                    brick.MoveMotor('C', 45);
                 end
-                    
+            
+% ------------------------------------------------------------------------------------------------        
 % COLOR DETECTION
     % Sensing the color red should result in a one second pause
     % Sensing the color blue should result in two beeps
     % Sensing the color green should result in three beeps
 
+                brick.SetColorMode(2);
+
     % Color Red
-                if color == 1
+                if (color == 1)
                     pause(1.0);
                 end
     
     % Color Blue
-                elseif color == 2
+                elseif (color == 2)
                     brick.beep();
                     pause(0.1);
                     brick.beep();
@@ -88,7 +100,7 @@ while 1
                 end    
 
     % Color Green           
-                elseif color == 3
+                elseif (color == 3)
                     brick.beep();
                     pause(0.1);
                     brick.beep();
