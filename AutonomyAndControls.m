9893cdaa7d866980c98a4bedad0795ed45c27441
Klaudia Sarat <112783872+klaudiasarat@users.noreply.github.com>
2023-03-29 15:29:47 -0700

    proper order implem; claw code and algos need work

--- AutonomyAndControls.m
+++ AutonomyAndControls.m
@@ -26,6 +26,7 @@ manualControls = false; % Not needed yet. Milestone 3
         % Turn left: leftarrow
         % Turn right: rightarrow
 
+
 % -------------------------------------------------------------------------
 % Vehicle will be moving autonomously until manualControls is true 
 % -------------------------------------------------------------------------
@@ -48,17 +49,16 @@ while 1
             setMotorSpeed('BC', 60);  % Vehicle will drive forward
             
             while(startMoving == 0)
-                touch = brick.TouchPressed(3);
-                distance = brick.UltrasonicDist(4);
 
                 brick.MoveMotor('BC', -50);
+
 
 % -------------------------------------------------------------------------
 % Distance and Maze Navigation 
 % // FIXME: Algorithm has to be corrected 
-% // FIXME: Simplify code
 % -------------------------------------------------------------------------
 
+    % Kill switch to exit loop
                 if touch == 1
 
                     brick.StopMotor('BC');
@@ -75,7 +75,6 @@ while 1
                     distance = brick.UltrasonicDist(4);
                     touch = brick.TouchPressed(3);
 
-
                 elseif distance > 40
                     pause(1);
                     brick.StopMotor('BC');
@@ -87,44 +86,6 @@ while 1
                     brick.MoveMotor('BC', -50);
                     pause(2);
 
-% -------------------------------------------------------------------------
-% Other cases:
-% -------------------------------------------------------------------------
-
-    % Autonomous program will run until key 'm' is pressed        
-        case 'm'
-            manualControls = true;
-            startMoving = 1;
-                
-    % Quit Program - Stop the Vehicle Completely
-        case 'q'
-            disp('Quit Program');
-            brick.StopMotor('B');
-            brick.StopMotor('C');
-            break;
-            
-    % Restart the Program
-        case 'r'
-            disp('Restart');
-            disp('Press up arrow');
-            startMoving = 0;
-
-% -------------------------------------------------------------------------
-% MANUAL CONTROLS
-% Manual controls will be used until key 'a' is pressed
-% ------------------------------------------------------------------------- 
-
-            while(key == 'm') && (key ~= 'a')
-
-    % Move Forward
-                if(key == uparrow)
-                    brick.MoveMotor('BC', 50);
-                end
-                
-    % Move Backward
-                if(key == downarrow)
-                    brick.MoveMotor('BC', -50);
-                end
 
 % ------------------------------------------------------------------------- 
 % COLOR DETECTION
@@ -137,19 +98,6 @@ while 1
     % color(2) = green values            
     % color(3) = blue values
 % ------------------------------------------------------------------------- 
-    
-        case 'uparrow'
-            while(startMoving == 0)
-                touch = brick.TouchPressed(3);
-
-                color = brick.ColorRGB(2);
-                brick.MoveMotor('BC', -50);
-                disp(color);
-
-    % Kill switch to exit loop
-                if touch == 1
-                    brick.StopMotor('BC');
-                    startMoving = 1;
 
     % Color Red
                 elseif (color(1) > 30 && color(2) < 20 && color(3) < 20)
@@ -177,6 +125,48 @@ while 1
                     brick.playTone(1000,500,200);
                     pause(1);
                     startMoving = 1;
+
+
+% -------------------------------------------------------------------------
+% Other cases:
+% // Question: Are we going to include case m just in case?
+% -------------------------------------------------------------------------
+
+    % Quit Program - Stop the Vehicle Completely
+        case 'q'
+            disp('Quit Program');
+            brick.StopMotor('B');
+            brick.StopMotor('C');
+            break;
+            
+    % Restart the Program
+        case 'r'
+            disp('Restart');
+            disp('Press up arrow');
+            startMoving = 0;
+
+
+% -------------------------------------------------------------------------
+% MANUAL CONTROLS
+% Manual controls will be used until key 'a' is pressed
+% // FIXME: Needs code still
+% ------------------------------------------------------------------------- 
+
+    % Autonomous program will run until key 'm' is pressed        
+        case 'm'
+            manualControls = true;
+            startMoving = 1;
+
+            while(key == 'm') && (key ~= 'a')
+
+    % Move Forward
+                if(key == uparrow)
+                    brick.MoveMotor('BC', 50);
+                end
+                
+    % Move Backward
+                if(key == downarrow)
+                    brick.MoveMotor('BC', -50);
                 end
             end
     end
